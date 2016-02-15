f12657f2d9b7646852e074871fc5eab6fdd74c0e
Ian Wagener <ianwagener@Ians-MacBook-Pro.local>
2016-02-14 18:36:13 -0800

    commented files

--- dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -1,15 +1,16 @@
 #include "my_functions.h"
-
+/*start of function*/
 void print_combination_1(void)
 {
   int i;
-  
+  /*printing 1-8*/
   for (i = '0'; i <= '8'; i++)
     {
       print_char(i);
       print_char(',');
       print_char(' ');
     }
+  /*printing 9*/
   if (i == '9')
     {
       print_char(i);
--- dreams_and_dedication_are_a_powerful_combination/2-print_combination_2.c
+++ dreams_and_dedication_are_a_powerful_combination/2-print_combination_2.c
@@ -1,22 +1,22 @@
 #include "my_functions.h"
-
+/*start of function*/
 void print_combination_2(void)
 {
   int i1;
   int i2;
-
+/*printing out all combinations of 2 digits*/
   for (i1 = '0'; i1 <= '8'; i1++) {
     for(i2 = '1'; i2 <= '9'; i2++) {
       if (i1 != i2) {
-	if (i2 >i1) {
-       	  print_char(i1);
-	   print_char(i2);
-	   if (i1 < '8' && i2 <= '9')
-	     {
-	       print_char(',');
-	       print_char(' ');  
-	     }
-	}
+        if (i2 >i1) {
+          print_char(i1);
+          print_char(i2);
+          if (i1 < '8' && i2 <= '9')
+          {
+            print_char(',');
+            print_char(' ');  
+          }
+        }
       }
     }    
   }
