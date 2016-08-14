c6641f860583de81a4a9f8d86b8c329bb32f6ef8
IanWagener <15@holbertonschool.com>
2016-08-14 11:30:11 -0700

    finished project

---
+++ n_trees/ntree_insert.c
@@ -0,0 +1,41 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <string.h>
+
+NTree *findnode(NTree *tree, char **parents);
+
+int ntree_insert(NTree **tree, char **parents, char *data)
+{
+  NTree *nuclear_parent;
+  List *ptr;
+  if (*tree == NULL) {
+    *tree = (NTree *) malloc(sizeof(NTree));
+    if (*tree == NULL) return 1;
+    (*tree)->str = strdup(data);
+    (*tree)->children = NULL;
+    return 0;
+  } else {
+    nuclear_parent = findnode(*tree, parents);
+    ptr = nuclear_parent->children;
+    nuclear_parent->children = (List *) malloc(sizeof(List));
+    nuclear_parent->children->next = ptr;
+    nuclear_parent->children->node = (NTree *) malloc(sizeof(NTree));
+    nuclear_parent->children->node->str = strdup(data);
+    nuclear_parent->children->node->children = NULL;
+  }
+  return 0;
+}
+
+NTree *findnode(NTree *tree, char **parents)
+{
+  List *ptr;
+  int i;
+  for (i=1 ; parents[i] != NULL ; i++) {
+    ptr = tree->children;
+    while (strcmp(ptr->node->str, parents[i]) != 0) {
+      ptr = ptr->next;
+    }
+    tree = ptr->node;
+  }
+  return tree;
+}
